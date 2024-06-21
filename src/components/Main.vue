ba25f85cecde294b63e982340f2a22228f308652
Davide-Morellato <morellato.davide@yahoo.it>
2024-06-21 16:02:34 +0200

    ADD: scrollTo() to nextPage methods and card-hover transition

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
+    <ProjectCard
+      v-for="project in projects"
+      :key="project.id"
+      :singleProject="project"
+    />
 
-    <ProjectCard v-for="project in projects" :key="project.id" :singleProject="project"/>
-
-    <div class="container py-5 d-flex flex-wrap flex-row justify-content-center gap-30">
-        <p :class="n === currentPage ? 'bg-softviolet' : 'bg-softpetrol'" class="page m-0 rounded-pill w-10px fw-bold text-center" @click="nextPage(n)" v-for="n in pageNumber" :key="n">
-            {{ n }}
-        </p>
+    <div
+      class="container py-5"
+    >
+      <ul class="d-flex flex-wrap flex-row justify-content-center gap-30">
+        <li
+          :class="n === currentPage ? 'bg-softviolet' : 'bg-softpetrol'"
+          class="page m-0 rounded-pill w-10px fw-bold text-center"
+          @click="nextPage(n)"
+          v-for="n in pageNumber"
+          :key="n"
+        >
+          {{ n }}
+        </li>
+      </ul>
     </div>
 
     <!-- <ul>
@@ -27,36 +40,39 @@
 
 <script>
 import axios from "axios";
-import ProjectCard from './ProjectCard.vue';
+import ProjectCard from "./ProjectCard.vue";
 
 export default {
-    components: {
-        ProjectCard,
-    },
+  components: {
+    ProjectCard,
+  },
   data() {
     return {
       projects: [],
       currentPage: 1,
-      pageNumber: null
+      pageNumber: null,
     };
   },
   methods: {
     fetchProjects() {
-      axios.get("http://127.0.0.1:8000/api/projects",
-        {params:{
-            page: this.currentPage
-        }}
-      ).then((result) => {
-        // console.log(result.data.projects.data);
-        this.projects = result.data.projects.data
-        this.pageNumber = result.data.projects.last_page
-      });
+      axios
+        .get("http://127.0.0.1:8000/api/projects", {
+          params: {
+            page: this.currentPage,
+          },
+        })
+        .then((result) => {
+          // console.log(result.data.projects.data);
+          this.projects = result.data.projects.data;
+          this.pageNumber = result.data.projects.last_page;
+        });
     },
 
-    nextPage(n){
-        this.currentPage = n
-        this.fetchProjects()
-    }
+    nextPage(n) {
+      this.currentPage = n;
+      this.fetchProjects();
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    },
   },
   created() {
     this.fetchProjects();
@@ -64,6 +80,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
